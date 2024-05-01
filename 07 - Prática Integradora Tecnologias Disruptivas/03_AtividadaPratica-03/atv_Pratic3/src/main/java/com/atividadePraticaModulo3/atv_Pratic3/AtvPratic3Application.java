36e4a845d94c99d693880a901fc04cde6f3872f5
Phelipe_Silvestre <phelipe17silvestre@gmail.com>
2024-05-01 16:47:42 -0300

    finalizando a aula 3 - Springboot

---
+++ 07 - Prática Integradora Tecnologias Disruptivas/03_AtividadaPratica-03/atv_Pratic3/src/main/java/com/atividadePraticaModulo3/atv_Pratic3/AtvPratic3Application.java
@@ -0,0 +1,13 @@
+package com.atividadePraticaModulo3.atv_Pratic3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtvPratic3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtvPratic3Application.class, args);
+	}
+
+}
---
+++ 07 - Prática Integradora Tecnologias Disruptivas/03_AtividadaPratica-03/atv_Pratic3/src/test/java/com/atividadePraticaModulo3/atv_Pratic3/AtvPratic3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atividadePraticaModulo3.atv_Pratic3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtvPratic3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
